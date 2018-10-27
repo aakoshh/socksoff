91e4c96940d087119a296441962a413b39d9d5c2
akosfa <akosfa@dunnhumby.com>
2018-10-27 20:53:48 +0100

    Overriding the socket creation so no connection is made.

--- server/src/main/scala/ServerMain.scala
+++ server/src/main/scala/ServerMain.scala
@@ -2,7 +2,7 @@ package com.socksoff.server
 
 import collection.JavaConverters._
 import com.typesafe.scalalogging.LazyLogging
-import java.net.Socket;
+import java.net.{Socket, InetAddress}
 import java.io.{InputStream, OutputStream, IOException}
 import scala.util.control.NonFatal
 import sockslib.server.io.{Pipe, SocketPipe, StreamPipe}
@@ -32,10 +32,33 @@ object ServerMain
 
   def makeSocks5Server = {
 
+    // Don't actually want to connect to the remote address.
+    class DummySocket(address: InetAddress, port: Int) extends Socket() {
+      override def getInetAddress(): InetAddress = address
+      override def getPort(): Int = port
+      override def getLocalAddress(): InetAddress = {
+        // The same address tehe base class returns in case of an error.
+        InetAddress.getByAddress(Array[Byte](0,0,0,0))
+      }
+      override def getLocalPort(): Int = 0
+    }
+
     class SmsSocketHandler() extends Socks5Handler {
+      override def makeRemoteSocket(address: InetAddress, port: Int): Socket = {
+        // Return a socket that the base handler can ask for the address
+        // but which doesn't actually connect.
+        new DummySocket(address, port)
+      }
+
       override def makeSocketPipe(socket1: Socket, socket2: Socket): SocketPipe = {
 
-        new SocketPipe(socket1, socket2) {
+        logger.info(s"Connecting to ${socket2.getInetAddress}:${socket2.getPort}")
+
+        // Make a real socket now.
+        val destination = new Socket(socket2.getInetAddress, socket2.getPort)
+
+        new SocketPipe(socket1, destination) {
+          // Will make two pipes to move the data between the two sockets in both directions.
           override def makeStreamPipe(in: InputStream, out: OutputStream, name: String): Pipe = {
 
             new StreamPipe(in, out, name) {
